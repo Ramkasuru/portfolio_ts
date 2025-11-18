a9315a7a7c4313500f7b1af42262c0910c23177a
Ram Kasuru <kodanda4u@gmail.com>
2025-11-18 01:52:31 +0000

    still not working and my website looks very bad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,29 +8,32 @@ import Research from "@/components/sections/research";
 import Contact from "@/components/sections/contact";
 import Footer from "@/components/shared/footer";
 import { SectionWrapper } from "@/components/ui/section-wrapper";
+import SmoothScroll from "@/components/ui/smooth-scroll";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-grow">
-        <Hero />
-        <SectionWrapper id="about" >
-          <About />
-        </SectionWrapper>
-        <SectionWrapper id="skills">
-          <Skills />
-        </SectionWrapper>
-        <SectionWrapper id="experience">
-          <Experience />
-        </SectionWrapper>
-        <Projects />
-        <Research />
-        <SectionWrapper id="contact">
-          <Contact />
-        </SectionWrapper>
-      </main>
-      <Footer />
-    </div>
+    <SmoothScroll>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Hero />
+          <SectionWrapper id="about" >
+            <About />
+          </SectionWrapper>
+          <SectionWrapper id="skills">
+            <Skills />
+          </SectionWrapper>
+          <SectionWrapper id="experience">
+            <Experience />
+          </SectionWrapper>
+          <Projects />
+          <Research />
+          <SectionWrapper id="contact">
+            <Contact />
+          </SectionWrapper>
+        </main>
+        <Footer />
+      </div>
+    </SmoothScroll>
   );
 }
