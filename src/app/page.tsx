bb6f01ddf76ae1414de8d1b0010f7408cbbd2a94
Ram Kasuru <kodanda4u@gmail.com>
2025-11-24 06:35:59 +0000

    Create deno.yml (1aaac95) main Nov 17, 2025, 9:48:43 PM Kasuru Kodanda R

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,32 +8,29 @@ import Research from "@/components/sections/research";
 import Contact from "@/components/sections/contact";
 import Footer from "@/components/shared/footer";
 import { SectionWrapper } from "@/components/ui/section-wrapper";
-import SmoothScroll from "@/components/ui/smooth-scroll";
 
 export default function Home() {
   return (
-    <SmoothScroll>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          <Hero />
-          <SectionWrapper id="about" >
-            <About />
-          </SectionWrapper>
-          <SectionWrapper id="skills">
-            <Skills />
-          </SectionWrapper>
-          <SectionWrapper id="experience">
-            <Experience />
-          </SectionWrapper>
-          <Projects />
-          <Research />
-          <SectionWrapper id="contact">
-            <Contact />
-          </SectionWrapper>
-        </main>
-        <Footer />
-      </div>
-    </SmoothScroll>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">
+        <Hero />
+        <SectionWrapper id="about" >
+          <About />
+        </SectionWrapper>
+        <SectionWrapper id="skills">
+          <Skills />
+        </SectionWrapper>
+        <SectionWrapper id="experience">
+          <Experience />
+        </SectionWrapper>
+        <Projects />
+        <Research />
+        <SectionWrapper id="contact">
+          <Contact />
+        </SectionWrapper>
+      </main>
+      <Footer />
+    </div>
   );
 }
